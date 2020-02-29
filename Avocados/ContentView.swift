3a4f831c742d52ebde5180ad382ad22e33d214b6
Tony Vu <52386267+tonyvu1@users.noreply.github.com>
2020-02-29 13:38:01 -0600

    Initial Commit

---
+++ Avocados/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Avocados
+//
+//  Created by Tony Vu on 2/29/20.
+//  Copyright © 2020 Tony Vu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
